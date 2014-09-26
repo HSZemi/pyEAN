1b8162b7ef35463a2d849bc0bf1417a48d0133fa
Sven Zemanek <hszemi@example.com>
2014-09-27 00:30:58 +0200

    small refactoring

--- makebarcode.py
+++ makebarcode.py
@@ -51,6 +51,52 @@ def check_ean(inputstring):
 	else:
 		return False
 
+def ean2bitstring(inputstring):
+	ean = check_ean(inputstring)
+	if not ean:
+		return False
+	
+	codings13 = list(range(10))
+	codings13[0] = "UUUUUURRRRRR"
+	codings13[1] = "UUGUGGRRRRRR"
+	codings13[2] = "UUGGUGRRRRRR"
+	codings13[3] = "UUGGGURRRRRR"
+	codings13[4] = "UGUUGGRRRRRR"
+	codings13[5] = "UGGUUGRRRRRR"
+	codings13[6] = "UGGGUURRRRRR"
+	codings13[7] = "UGUGUGRRRRRR"
+	codings13[8] = "UGUGGURRRRRR"
+	codings13[9] = "UGGUGURRRRRR"
+
+	codings = list(range(10))
+	codings[0] = ("0001101","0100111","1110010")
+	codings[1] = ("0011001","0110011","1100110")
+	codings[2] = ("0010011","0011011","1101100")
+	codings[3] = ("0111101","0100001","1000010")
+	codings[4] = ("0100011","0011101","1011100")
+	codings[5] = ("0110001","0111001","1001110")
+	codings[6] = ("0101111","0000101","1010000")
+	codings[7] = ("0111011","0010001","1000100")
+	codings[8] = ("0110111","0001001","1001000")
+	codings[9] = ("0001011","0010111","1110100")
+
+	code = ""
+
+	for i in range(len(codings13[ean[0]])):
+		c = codings13[ean[0]][i]
+		idx = 0
+		if(c == "U"):
+			idx = 0
+		elif(c == "G"):
+			idx = 1
+		elif(c == "R"):
+			idx = 2
+		code += codings[ean[i+1]][idx]
+
+	code = "101" + code[:42] + "01010" + code[42:] + "101"
+	return code
+
+### Main
 
 if(len(sys.argv) != 2):
 	print("Usage: {0} [EAN]".format(sys.argv[0]))
@@ -61,46 +107,7 @@ if(len(sys.argv[1]) != 13 or not ean):
 	print("Usage: {0} [EAN]".format(sys.argv[0]))
 	sys.exit(1)
 
-codings13 = list(range(10))
-codings13[0] = "UUUUUURRRRRR"
-codings13[1] = "UUGUGGRRRRRR"
-codings13[2] = "UUGGUGRRRRRR"
-codings13[3] = "UUGGGURRRRRR"
-codings13[4] = "UGUUGGRRRRRR"
-codings13[5] = "UGGUUGRRRRRR"
-codings13[6] = "UGGGUURRRRRR"
-codings13[7] = "UGUGUGRRRRRR"
-codings13[8] = "UGUGGURRRRRR"
-codings13[9] = "UGGUGURRRRRR"
-
-codings = list(range(10))
-codings[0] = ("0001101","0100111","1110010")
-codings[1] = ("0011001","0110011","1100110")
-codings[2] = ("0010011","0011011","1101100")
-codings[3] = ("0111101","0100001","1000010")
-codings[4] = ("0100011","0011101","1011100")
-codings[5] = ("0110001","0111001","1001110")
-codings[6] = ("0101111","0000101","1010000")
-codings[7] = ("0111011","0010001","1000100")
-codings[8] = ("0110111","0001001","1001000")
-codings[9] = ("0001011","0010111","1110100")
-
-code = ""
-
-for i in range(len(codings13[ean[0]])):
-	c = codings13[ean[0]][i]
-	idx = 0
-	if(c == "U"):
-		idx = 0
-	elif(c == "G"):
-		idx = 1
-	elif(c == "R"):
-		idx = 2
-	code += codings[ean[i+1]][idx]
-
-code2 = "101 " + code[:42] + " 01010 " + code[42:] + " 101"
-code = "101" + code[:42] + "01010" + code[42:] + "101"
-print(code2)
+code = ean2bitstring(sys.argv[1])
 
 f = open("barcode.svg", "w")
 print(svgheadtemplate.format(102, 34), file=f)
